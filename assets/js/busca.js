a94e7b285089cfd78dd98d6da63a7a027da8bbdd
RodrigoSouzza <contato.rodrigoom@gmail.com>
2024-08-26 20:19:55 -0300

    EVOLUÇÃO - desenvolvimento da seção de pesquisa

---
+++ assets/js/busca.js
@@ -0,0 +1,48 @@
+const pesquisaInput = document.querySelector("#pesquisa")
+const minifigureList = document.querySelector("#minifigures_list")
+const btnBusca = document.getElementById("buscar")
+
+function exibirBusca(){
+    
+    minifigureList.innerHTML = ""
+
+    let minifigureData = JSON.parse(localStorage.getItem("minifiguresList"))
+    
+    minifigureData.forEach((minifigure) =>{
+
+        if(pesquisaInput.value == minifigure.nome) {            
+
+            const newMinifigure = document.createElement("div")
+            newMinifigure.className = "minifigure"
+
+            const imageMinifigure = document.createElement("img")
+
+            imageMinifigure.setAttribute("src", minifigure.url)        
+        
+            const nomeMinifigure = document.createElement("h2")
+            nomeMinifigure.innerText = minifigure.nome
+
+            const colecaoMinifigure = document.createElement("p")
+            colecaoMinifigure.innerText = minifigure.colecao
+
+            const statusMinifigure = document.createElement("span")
+
+            if(minifigure.status == "ja-tenho"){
+                statusMinifigure.className = "situacao-comprado"
+                statusMinifigure.innerText = "já tenho"
+            }else {
+                statusMinifigure.className = "situacao"
+                statusMinifigure.innerText = "não tenho"
+            }
+        
+            newMinifigure.appendChild(imageMinifigure)
+            newMinifigure.appendChild(nomeMinifigure)
+            newMinifigure.appendChild(colecaoMinifigure)
+            newMinifigure.appendChild(statusMinifigure)
+
+            minifigureList.appendChild(newMinifigure)
+        }       
+    })    
+}
+
+btnBusca.addEventListener("click", exibirBusca)
